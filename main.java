ac234fd697dc487e95394ea8fff2957cda89c7a2
S.M Farhan Labib <shahmdfarhan123@gmail.com>
2026-08-13 22:18:04 +0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main{
+public static void main(String[] args) {
+    double radius=7.5;
+    double area=Math.PI * radius * radius;
+    double perimeter=2 * Math.PI * radius;
+    System.out.println("Area= "+ area);
+    System.out.println("Perimeter= "+ perimeter);
+    
+}
+
+
+
+
+
+
+}
