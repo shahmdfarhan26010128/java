467527d9b08682e5e71fe3df850aacbe6792b219
S.M Farhan Labib <shahmdfarhan123@gmail.com>
2026-08-15 21:35:48 +0600

    Add files via upload

---
+++ average.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class average {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a, b , c;
+    a= sc.nextInt();
+    b= sc.nextInt();
+    c= sc.nextInt();
+int avg=(a+b+c)/3;
+System.out.println(avg);
+
+
+    
+   } 
+    
+}
